a4ce884b738c99018423a9264a13550f980377ca
sahar ben baruch <saharbenbaruch@gmail.com>
2019-11-13 21:13:13 +0200

    including readme.txt

--- .idea/Main.py
+++ .idea/Main.py
@@ -8,6 +8,7 @@ global numOfNodes
 
 def load_graph(path):
    file = pd.read_csv(path)
+   #numOfEdges=0  #for evaluation!!!
    df = pd.DataFrame(file, columns=None)
    graph1=graph()
    listOfNodes=[]
@@ -21,6 +22,7 @@ def load_graph(path):
                dest=int (dest)
                node1=node(source)
                node1.addNeighbor(dest)
+               #numOfEdges=numOfEdges+1 #for evaluation!!!
                listOfNodes.append(source)
                listOfNodes.append(dest)
                graph1.nodes[source] = node1
@@ -32,11 +34,13 @@ def load_graph(path):
             if ( int(line[0]) not in graph1.getNodes()):
                 node1=node(str(line[0]))
                 node1.addNeighbor(line[1])
+                #numOfEdges = numOfEdges + 1  #for evaluation!!!
                 #node1.pageRank=i  // just for test.
                 graph1.nodes[line[0]]=node1
             #in case node exist , add new neighbor
             else:
                 graph1.nodes[line[0]].addNeighbor(line[1])
+                #numOfEdges = numOfEdges + 1  #for evaluation!!!
 
             #check if source in listOfNodes, if not -add
             if (line[0] not in listOfNodes):
@@ -48,13 +52,14 @@ def load_graph(path):
         i=i+1
    globals()['ourGraph']=graph1
    ourGraph.numOfNodes=len(listOfNodes)
-   #print (globals() ['numOfNodes'])
+   #print (ourGraph.numOfNodes)  # for evaluation!!!
+   #print (numOfEdges)  #for evaluation!!!
 
 
 
 def main():
     load_graph("C:\\Users\\Sahar Ben Baruch\\Desktop\\Wikipedia_votes.csv")
-    ourGraph.calculate_page_rank (0.85, 0.001, 1)
-    print (ourGraph.get_all_PageRank())
+    ourGraph.calculate_page_rank (0.85, 0.001, 20)
+    print (ourGraph.get_top_nodes(10))
 if __name__ == "__main__":
     main()
---
+++ readme.txt
@@ -0,0 +1,30 @@
+This API implements Page Rank algorithm for directed unweighted graph.
+Our API expect getting .csv file contain <source id , destination id> without headers.
+
+We created class Node - contains 'name' , 'pageRank' , 'dest' (represent all nodes 
+that current node has edges to them.)
+
+we created class for Graph- contains 'numOfNodes' and dictionary of 'Nodes'
+
+Instructions:
+
+load_graph(path) for read the dataset and creating graph out of it.
+This function collects all new nodes its occur and add them to list of all nodes so we could
+know how many unique nodes the graph contains.
+
+The function breaks every line to source and destination nodes and create objects of them.
+We use 'addNeighbor(Node)'  to add destination node to source node.
+
+we add all nodes to graph .
+The graph is global variable .
+
+calculate_page_rank (beta, epsilon, maxIteration) - generate by graph object.
+Its run over all nodes from nodes dictionary and use different dictionary calls 'calculateTable'
+to save the sum contribution of current node (in act of source) in page rank of destination node.
+
+for example, node A has edge to node B.
+A' rank is 5  -> B will get 1/5 * a'rank * beta.
+
+in the end of iteration, we split between all nodes the extra rank left( in case of beta<1).
+
+we check that total epsilon is lower than given epsilon, else - break.
